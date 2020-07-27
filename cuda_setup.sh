df2979479e49992a21d016e7b5818d1395dd93e5
VIshnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2020-07-27 09:07:39 +0200

    Update cuda for Ubuntu 20.04

--- cuda_setup.sh
+++ cuda_setup.sh
@@ -1,41 +1,38 @@
-# #!/bin/bash
+#!/bin/bash
 
-# echo "[Update the package lists and upgrade them]"
-# sudo apt update
-# sudo apt upgrade -y
+echo "[Update the package lists and upgrade them]"
+sudo apt update
+sudo apt upgrade -y
 
-# echo "[Create installation folder]"
-# mkdir ~/cuda_install
-# cd ~/cuda_install
+echo "[Create installation folder]"
+mkdir ~/cuda_install
+cd ~/cuda_install
 
-# echo "[Pre-installation]"
-# sudo apt-get install linux-headers-$(uname -r)
+echo "[Pre-installation]"
+sudo apt-get install linux-headers-$(uname -r)
 
-# echo "[Install Cuda 10.2]"
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-# sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-# wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-# sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-# sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
-# sudo apt-get update
-# sudo apt-get -y install cuda
+echo "[Install Cuda 10.2]"
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
 
-# echo "[Blacklist nouveau]"
-# if [ ! -e /etc/modprobe.d/blacklist-nouveau.conf ]; then
-#   sudo sh -c 'echo "blacklist nouveau" > /etc/modprobe.d/blacklist-nouveau.conf'
-#   sudo sh -c 'echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf'
-# fi
-# sudo update-initramfs -u
+echo "[Blacklist nouveau]"
+if [ ! -e /etc/modprobe.d/blacklist-nouveau.conf ]; then
+  sudo sh -c 'echo "blacklist nouveau" > /etc/modprobe.d/blacklist-nouveau.conf'
+  sudo sh -c 'echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf'
+fi
+sudo update-initramfs -u
 
-# echo "[Path Setup]"
-# sudo sh -c 'echo "export PATH=$PATH:/usr/local/cuda-10.2/bin:/usr/local/cuda-10.2/NsightCompute-2019.1${PATH:+:${PATH}}" > /etc/profile.d/cuda.conf'
-# sudo sh -c 'echo "export CUDADIR=/usr/local/cuda-10.2" >> /etc/profile.d/cuda.conf'
-# sudo chmod +x /etc/profile.d/cuda.conf
-# sudo ldconfig
-# export PATH=/usr/local/cuda-10.2/bin:/usr/local/cuda-10.2/NsightCompute-2019.1${PATH:+:${PATH}}
-# export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64\
-#                          ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-# sudo systemctl enable nvidia-persistenced
+echo "[Path Setup]"
+export PATH=/usr/local/cuda-11.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64\
+                         ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64
+sudo systemctl enable nvidia-persistenced
 
-# echo "[Dependency installation]"
-# sudo apt-get install -y g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+echo "[Dependency installation]"
+sudo apt-get install -y g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
--- setup_ros2.sh
+++ setup_ros2.sh
@@ -19,7 +19,41 @@ if [ ! -e /etc/apt/sources.list.d/ros2-latest.list ]; then
 fi
 
 echo "[Install ROS 2 packages]"
-sudo apt update
+sudo apt update 
+sudo apt install -y build-essential \
+  cmake \
+  git \
+  libbullet-dev \
+  python3-colcon-common-extensions \
+  python3-flake8 \
+  python3-pip \
+  python3-pytest-cov \
+  python3-rosdep \
+  python3-setuptools \
+  python3-vcstool \
+  wget
+# install some pip packages needed for testing
+python3 -m pip install -U \
+  argcomplete \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest
+# install Fast-RTPS dependencies
+sudo apt install --no-install-recommends -y \
+  libasio-dev \
+  libtinyxml2-dev
+# install Cyclone DDS dependencies
+sudo apt install --no-install-recommends -y \
+  libcunit1-dev
+# ROS2 Installation.
 sudo apt install -y ros-foxy-desktop
 source /opt/ros/foxy/setup.bash
 sudo apt update
